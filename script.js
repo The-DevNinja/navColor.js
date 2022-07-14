2ea4ab47530447b328e56978882c10d522be9ccc
laaouatni <laaouatni.anas@outlook.it>
2022-07-14 16:35:00 +0200

    third beta with a little bug

--- script.js
+++ script.js
@@ -1,88 +1,137 @@
-let iconStyleArray = Object.values({
-    outlined: "fa-regular",
-    filled: "fa-solid"
+createNav(iconData, {
+    "navElement": "#nav"
 });
 
-let navContainer = document.querySelector("nav");
-let parentClass = "icon-div";
+function createNav(arrayIcons, options) {
+    let bodyElement = document.querySelector("body");
+    let navContainer = setNavElement();
 
-iconData.forEach((thisData) => {
-    let newIconContainer;
-    let thisIconName = thisData.iconName;
-
-    createDivContainer(parentClass);
-    createNewIcon(thisIconName);
-    appendChildToNavbar(navContainer);
-
-
-    function createDivContainer(className) {
-        newIconContainer = document.createElement("div");
-        newIconContainer.classList.add(className);
-    }
-
-    function createNewIcon(iconName) {
-        let newIcon = document.createElement("i");
-
-        let classesToAdd = [`${iconName}`, `${iconStyleArray[0]}`, `icon`];
-        classesToAdd.forEach((thisClass) => {
-            newIcon.classList.add(`${thisClass}`);
-        });
-
-        newIconContainer.appendChild(newIcon);
-    }
-
-    function appendChildToNavbar(navName) {
-        navName.appendChild(newIconContainer);
-    }
-});
-
-let allIconsDiv = document.querySelectorAll(`.${parentClass}`);
-
-allIconsDiv.forEach((thisIconDiv, index) => {
-    let thisIcon = thisIconDiv.querySelector(".icon");
-
-    addDataAttribute(thisIconDiv);
-    thisIconDiv.addEventListener("click", styleIcon);
-
-    function styleIcon() {
-        changeBgColor();
-        makeIconSolid(thisIcon);
-
-        function changeBgColor() {
-            let thisIconColor = iconData[index].color;
-            let bodyElement = document.querySelector("body");
-
-            bodyElement.style.setProperty("--bg", `${thisIconColor}`);
-        }
-
-        function makeIconSolid(iconName) {
-            resetAllIcons();
-            addClassesToThisIcon();
+    let iconStyleArray = Object.values({
+        outlined: "fa-regular",
+        filled: "fa-solid"
+    });
     
-            function resetAllIcons() {
-                allIconsDiv.forEach((thisDiv) => {
-                    let thisSolidIcon = thisDiv.querySelector(".icon");
-    
-                    makeIconOutlined(thisSolidIcon);
-                    thisDiv.classList.remove("active");
-                    
-                    function makeIconOutlined(iconName) {
-                        iconName.classList.remove(`${iconStyleArray[1]}`);
-                        iconName.classList.add(`${iconStyleArray[0]}`);
-                    }
-                });
+    function setNavElement() {
+        let element;  
+
+        if(options.navElement) {
+            createNavWithOptions(options);
+
+            function createNavWithOptions(options) {
+                element = document.createElement("nav");
+                bodyElement.appendChild(element);
+                
+                let firstLetter = options.navElement.charAt(0);
+                
+                let isID = firstLetter == "#";
+                let isClass = firstLetter == ".";
+
+                if(isID) {
+                    element.id = options.navElement.replace("#", "");
+                } else if(isClass) {
+                    element.className.add(options.navElement.replace(".", ""));
+                } else {
+                    element.id = options.navElement; 
+                }
             }
+        } else {
+            element = document.querySelector("nav");
 
-            function addClassesToThisIcon() {
-                thisIconDiv.classList.add("active");
-                iconName.classList.add(`${iconStyleArray[1]}`);
+            if(!element) { 
+
+                createNormalNav();
+                function createNormalNav() {
+                    element = document.createElement("nav");
+                    bodyElement.appendChild(element);
+                }
             }
         }
-    }
 
-    function addDataAttribute(iconName) {
-        let thisTitle = iconData[index].text;
-        iconName.setAttribute("data-text", `${thisTitle}`);
+        return element;
     }
-});
+    
+    let parentClass = "icon-div";
+    
+    iconData.forEach((thisData) => {
+        let newIconContainer;
+        let thisIconName = thisData.iconName;
+    
+        createDivContainer(parentClass);
+        createNewIcon(thisIconName);
+        appendChildToNavbar(navContainer);
+    
+    
+        function createDivContainer(className) {
+            newIconContainer = document.createElement("div");
+            newIconContainer.classList.add(className);
+        }
+    
+        function createNewIcon(iconName) {
+            let newIcon = document.createElement("i");
+    
+            let classesToAdd = [`${iconName}`, `${iconStyleArray[0]}`, `icon`];
+            classesToAdd.forEach((thisClass) => {
+                newIcon.classList.add(`${thisClass}`);
+            });
+    
+            newIconContainer.appendChild(newIcon);
+        }
+    
+        function appendChildToNavbar(navName) {
+            navName.appendChild(newIconContainer);
+        }
+    });
+    
+    let allIconsDiv = document.querySelectorAll(`.${parentClass}`);
+    
+    allIconsDiv.forEach((thisIconDiv, index) => {
+        let thisIcon = thisIconDiv.querySelector(".icon");
+    
+        addDataAttribute(thisIconDiv);
+        thisIconDiv.addEventListener("click", styleIcon);
+    
+        function styleIcon() {
+            changeBgColor();
+            makeIconSolid(thisIcon);
+    
+            function changeBgColor() {
+                let thisIconColor = iconData[index].color;
+                
+    
+                bodyElement.style.setProperty("--bg", `${thisIconColor}`);
+            }
+    
+            function makeIconSolid(iconName) {
+                resetAllIcons();
+                addClassesToThisIcon();
+        
+                function resetAllIcons() {
+                    allIconsDiv.forEach((thisDiv) => {
+                        let thisSolidIcon = thisDiv.querySelector(".icon");
+        
+                        makeIconOutlined(thisSolidIcon);
+                        thisDiv.classList.remove("active");
+                        
+                        function makeIconOutlined(iconName) {
+                            iconName.classList.remove(`${iconStyleArray[1]}`);
+                            iconName.classList.add(`${iconStyleArray[0]}`);
+                        }
+                    });
+                }
+    
+                function addClassesToThisIcon() {
+                    thisIconDiv.classList.add("active");
+                    iconName.classList.add(`${iconStyleArray[1]}`);
+                }
+            }
+        }
+    
+        function addDataAttribute(iconName) {
+            let thisTitle = iconData[index].text;
+            iconName.setAttribute("data-text", `${thisTitle}`);
+        }
+    });
+
+}    
+
 
